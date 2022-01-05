d7cca968c055d4429a443d9d70e37a3aeb13ef5e
microwavedcola1 <microwavedcola@gmail.com>
2022-01-05 22:38:38 +0100

    use helper
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- programs/dasheri/src/lib.rs
+++ programs/dasheri/src/lib.rs
@@ -1,11 +1,11 @@
 use anchor_lang::prelude::*;
 use mango::instruction::MangoInstruction;
-use solana_program::instruction::AccountMeta;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod dasheri {
+    use mango::instruction;
     use solana_program::program::invoke;
 
     use super::*;
@@ -14,23 +14,18 @@ pub mod dasheri {
         ctx: Context<CreateMangoAccount>,
         account_num: u64,
     ) -> ProgramResult {
+        let instruction = instruction::create_mango_account(
+            ctx.accounts.mango_program_ai.key,
+            ctx.accounts.mango_group.to_account_info().key,
+            ctx.accounts.mango_account.to_account_info().key,
+            ctx.accounts.owner.to_account_info().key,
+            ctx.accounts.system_program.to_account_info().key,
+            account_num,
+        )
+        .unwrap();
+
         invoke(
-            &solana_program::instruction::Instruction {
-                program_id: *ctx.accounts.mango_program_ai.key,
-                accounts: vec![
-                    AccountMeta::new(*ctx.accounts.mango_group.to_account_info().key, false),
-                    AccountMeta::new(*ctx.accounts.mango_account.to_account_info().key, false),
-                    AccountMeta::new(*ctx.accounts.owner.to_account_info().key, true),
-                    AccountMeta::new_readonly(
-                        *ctx.accounts.system_program.to_account_info().key,
-                        false,
-                    ),
-                ],
-                data: MangoInstruction::CreateMangoAccount {
-                    account_num: account_num,
-                }
-                .pack(),
-            },
+            &instruction,
             &[
                 ctx.accounts.mango_program_ai.to_account_info().clone(),
                 ctx.accounts.mango_group.to_account_info().clone(),
