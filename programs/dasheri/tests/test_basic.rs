14bb622a04805a7ade214f6d05c37dc802996b3e
microwavedcola1 <microwavedcola@gmail.com>
2022-01-19 15:44:57 +0100

    bring mango test code
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- programs/dasheri/tests/test_basic.rs
+++ programs/dasheri/tests/test_basic.rs
@@ -3,8 +3,10 @@ use mango::state::{MangoGroup, NodeBank, RootBank, QUOTE_INDEX};
 use solana_program::instruction::Instruction;
 use solana_program::pubkey::Pubkey;
 use solana_program_test::*;
+use solana_sdk::signature::Keypair;
 use solana_sdk::signer::Signer;
 
+use crate::cookies::MangoGroupCookie;
 use program_test::*;
 
 mod program_test;
@@ -13,27 +15,35 @@ mod program_test;
 #[tokio::test]
 async fn test_basic() {
     // Setup
-    let mut context = TestContext::new().await;
-    let mango_group_cookie = MangoGroupCookie::default(&mut context).await;
+    let config = MangoProgramTestConfig::default();
+    let mut test = MangoProgramTest::start_new(&config).await;
+    let mut mango_group_cookie = MangoGroupCookie::default(&mut test).await;
+    MangoGroupCookie::full_setup(
+        &mut mango_group_cookie,
+        &mut test,
+        config.num_users,
+        config.num_mints - 1,
+    )
+    .await;
 
     // Create mango account
     const ACCOUNT_NUM: u64 = 0_u64;
     let (mango_account, _) = Pubkey::find_program_address(
         &[
             &mango_group_cookie.address.as_ref(),
-            &&context.solana.context.borrow().payer.pubkey().as_ref(),
+            &test.context.payer.pubkey().as_ref(),
             &ACCOUNT_NUM.to_le_bytes(),
         ],
-        &context.mango.program_id,
+        &test.mango_program_id,
     );
     let instructions = vec![Instruction {
-        program_id: context.dasheri.program_id,
+        program_id: test.dasheri_program_id,
         accounts: anchor_lang::ToAccountMetas::to_account_metas(
             &dasheri::accounts::CreateMangoAccount {
-                mango_program: context.mango.program_id,
+                mango_program: test.mango_program_id,
                 mango_group: mango_group_cookie.address,
                 mango_account: mango_account,
-                owner: context.solana.context.borrow().payer.pubkey(),
+                owner: test.context.payer.pubkey(),
                 system_program: solana_sdk::system_program::id(),
             },
             None,
@@ -42,15 +52,16 @@ async fn test_basic() {
             account_num: ACCOUNT_NUM,
         }),
     }];
-    context
-        .solana
-        .process_transaction(&instructions, Some(&[]))
+    test.process_transaction(&instructions, Some(&[]))
         .await
         .unwrap();
 
+    // Update cache
+    mango_group_cookie.run_keeper(&mut test).await;
+
     // Deposit
-    let test_quote_mint_index = context.mints.len() - 1;
-    let mango_group = context
+    let test_quote_mint_index = test.mints.len() - 1;
+    let mango_group = test
         .load_account::<MangoGroup>(mango_group_cookie.address)
         .await;
     println!(
@@ -58,23 +69,23 @@ async fn test_basic() {
         mango_group.tokens[QUOTE_INDEX].root_bank
     );
     let root_bank_pk = mango_group.tokens[QUOTE_INDEX].root_bank;
-    let root_bank = context.load_account::<RootBank>(root_bank_pk).await;
+    let root_bank = test.load_account::<RootBank>(root_bank_pk).await;
     let node_bank_pk = root_bank.node_banks[0];
-    let node_bank = context.load_account::<NodeBank>(node_bank_pk).await;
+    let node_bank = test.load_account::<NodeBank>(node_bank_pk).await;
 
     let instructions = vec![Instruction {
-        program_id: context.dasheri.program_id,
+        program_id: test.dasheri_program_id,
         accounts: anchor_lang::ToAccountMetas::to_account_metas(
             &dasheri::accounts::Deposit {
-                mango_program: context.mango.program_id,
+                mango_program: test.mango_program_id,
                 mango_group: mango_group_cookie.address,
                 mango_cache: mango_group.mango_cache.key(),
                 root_bank: root_bank_pk,
                 node_bank: node_bank_pk,
                 node_bank_vault: node_bank.vault.key(),
-                owner_token_account: context.users[0].token_accounts[test_quote_mint_index].key(),
+                owner_token_account: test.token_accounts[test_quote_mint_index].key(),
                 mango_account: mango_account,
-                owner: context.users[0].key.pubkey(),
+                owner: test.users[0].pubkey(),
                 system_program: solana_sdk::system_program::id(),
                 token_program: spl_token::id(),
             },
@@ -82,9 +93,12 @@ async fn test_basic() {
         ),
         data: anchor_lang::InstructionData::data(&dasheri::instruction::Deposit { quantity: 100 }),
     }];
-    context
-        .solana
-        .process_transaction(&instructions, Some(&[&context.users[0].key]))
-        .await
-        .unwrap();
+    test.process_transaction(
+        &instructions,
+        Some(&[&Keypair::from_base58_string(
+            &test.users[0].to_base58_string(),
+        )]),
+    )
+    .await
+    .unwrap();
 }
