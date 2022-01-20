a1ab666869ae74efe34e4d5d990e7a0cbf16a362
microwavedcola1 <microwavedcola@gmail.com>
2022-01-20 09:58:31 +0100

    some more ixs
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- programs/dasheri/src/state/pool.rs
+++ programs/dasheri/src/state/pool.rs
@@ -3,6 +3,10 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(Default)]
 pub struct Pool {
+    pub bump: u8,
+    pub deposit_mint: Pubkey,
     pub vault: Pubkey,
+    pub admin: Pubkey,
+    pub padding: [u8; 31],
 }
-const_assert!(std::mem::size_of::<Pool>() == 32);
+const_assert!(std::mem::size_of::<Pool>() == 1 + 32 + 32 + 32 + 31);
--- programs/dasheri/src/state/pool_account.rs
+++ programs/dasheri/src/state/pool_account.rs
@@ -3,6 +3,9 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(Default)]
 pub struct PoolAccount {
+    pub bump: u8,
     pub pool: Pubkey,
+    pub owner: Pubkey,
+    pub padding: [u8; 31],
 }
-const_assert!(std::mem::size_of::<PoolAccount>() == 32);
+const_assert!(std::mem::size_of::<PoolAccount>() == 1 + 32 + 32 + 31);
